92c3879e80d609e6651ea05b92bad66698652d29
J. Degand <70610011+jdegand@users.noreply.github.com>
2025-07-09 01:50:47 +0000

    refactor: add new route that has redirectTo function

--- apps/angular/60-async-redirect/src/app/dashboard.ts
+++ apps/angular/60-async-redirect/src/app/dashboard.ts
@@ -1,6 +1,5 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { Router, RouterLink, RouterOutlet } from '@angular/router';
-import { UserProfileService } from './user-profile.service';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,8 +12,8 @@ import { UserProfileService } from './user-profile.service';
       </button>
       <button
         class="rounded border border-black bg-white px-4 py-2 text-black hover:bg-gray-200"
-        (click)="navigate()">
-        User Page
+        routerLink="/detail">
+        Detail
       </button>
     </div>
     <router-outlet />
@@ -22,13 +21,4 @@ import { UserProfileService } from './user-profile.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [RouterOutlet, RouterLink],
 })
-export class Dashboard {
-  private router = inject(Router);
-  private userProfile = inject(UserProfileService);
-
-  navigate() {
-    this.userProfile.getProfile().subscribe((profile) => {
-      void this.router.navigate(['/', profile]);
-    });
-  }
-}
+export class Dashboard {}
--- apps/angular/60-async-redirect/src/app/routes.ts
+++ apps/angular/60-async-redirect/src/app/routes.ts
@@ -1,17 +1,30 @@
+import { inject } from '@angular/core';
 import { Routes } from '@angular/router';
 import { AdminPage } from './admin-page';
 import { App } from './app';
 import { Dashboard } from './dashboard';
 import { ProfilePage } from './profile-page';
 import { UserPage } from './user-page';
+import { UserProfileService } from './user-profile.service';
 
 export const routes: Routes = [
   {
     path: '',
     component: App,
     children: [
-      { path: '', pathMatch: 'full', component: Dashboard },
+      {
+        path: '',
+        pathMatch: 'full',
+        component: Dashboard,
+      },
       { path: 'profile', component: ProfilePage },
+      {
+        path: 'detail',
+        redirectTo: () => {
+          const userProfileService = inject(UserProfileService);
+          return userProfileService.getProfile();
+        },
+      },
       { path: 'admin', component: AdminPage },
       { path: 'user', component: UserPage },
     ],
